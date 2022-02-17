23fed8791d464c27b0581f1e34af69116c818e97
Christoph Vögele <christoph.voegele@fhnw.ch>
2022-02-17 11:53:59 +0100

    update build

--- bdsf/build.sh
+++ bdsf/build.sh
@@ -1,5 +1,5 @@
+(cd $BUILD_PREFIX/bin && ln -s $FC gfortran)
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  (cd $BUILD_PREFIX/bin && ln -s $FC gfortran)
   export LDFLAGS="$LDFLAGS -shared"
 elif [[ $OSTYPE == 'darwin'* ]]; then
   echo export LIBRARY_PATH="$LIBRARY_PATH:/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib"
