a0ecda84dd3517ce6cad22176b9b083b5121de0e
Christoph Vögele <christoph.voegele@fhnw.ch>
2022-02-18 13:28:07 +0100

    update build

--- bdsf/build.sh
+++ bdsf/build.sh
@@ -5,7 +5,6 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
 elif [[ $OSTYPE == 'darwin'* ]]; then
   #export LDFLAGS="$LDFLAGS -L /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib"
-  export CFLAGS="$CFLAGS -march=cascadelake"
 fi
 
 $PYTHON -m pip install . --no-deps -vv
