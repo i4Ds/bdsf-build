9684be07e8c7ca07691cf21947188dfd7d6071d3
Christoph Vögele <christoph.voegele@fhnw.ch>
2022-02-18 11:07:49 +0100

    update build

--- bdsf/build.sh
+++ bdsf/build.sh
@@ -1,8 +1,11 @@
-(cd $BUILD_PREFIX/bin && ln -s $FC gfortran)
+
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  (cd $BUILD_PREFIX/bin && ln -s $FC gfortran)
   export LDFLAGS="$LDFLAGS -shared"
+
 elif [[ $OSTYPE == 'darwin'* ]]; then
-  echo export LIBRARY_PATH="$LIBRARY_PATH:/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib"
+  ls $PREFIX/lib
+  export LDFLAGS="$LDFLAGS -L /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib"
 fi
 
 $PYTHON -m pip install . --no-deps -vv
